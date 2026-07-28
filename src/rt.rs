854b4f3d638d0d094eca9b67bff3b72d5519bc78
ltungv <ltungv.git@gmail.com>
2026-07-28 23:05:22 +0200

    wip: trap handling with context switch
    + no memory has been allocated for the trap frame
    + mscratch still needs to be updated to contain the address to the memory region of a `TrapFrame`

--- src/rt.rs
+++ src/rt.rs
@@ -76,6 +76,135 @@ extern "C" fn kmain() {
         drop(v3);
         drop(v4);
     }
+
+    println!("triggering faults...");
+    unsafe {
+        // Set the next machine timer to fire.
+        let mtimecmp = 0x0200_4000 as *mut u64;
+        let mtime = 0x0200_bff8 as *const u64;
+        // The frequency given by QEMU is 10_000_000 Hz, so this sets
+        // the next interrupt to fire one second from now.
+        mtimecmp.write_volatile(mtime.read_volatile() + 10_000_000);
+
+        // Let's cause a page fault and see what happens. This should trap
+        // to m_trap under trap.rs
+        let v = 0x0 as *mut u64;
+        v.write_volatile(0);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct TrapFrame {
+    gp_regs: [usize; 32],
+    fp_regs: [usize; 32],
+    satp: usize,
+    pc: usize,
+    hartid: usize,
+    qm: usize,
+    pid: usize,
+    mode: usize,
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn mtrap(
+    epc: usize,
+    tval: usize,
+    cause: usize,
+    hartid: usize,
+    status: usize,
+    frame: *mut TrapFrame,
+) -> usize {
+    let is_async = cause >> 63 & 1 == 1;
+    // The cause contains the type of trap (sync, async) as well as the cause
+    // number. So, here we narrow down just the cause number.
+    let cause_code = cause & 0xfff;
+    let mut program_counter = epc;
+    if is_async {
+        // Asynchronous trap
+        match cause_code {
+            3 => {
+                // Machine software
+                println!("Machine software interrupt CPU#{}", hartid);
+            }
+            7 => unsafe {
+                // Machine timer
+                let mtimecmp = 0x0200_4000 as *mut u64;
+                let mtime = 0x0200_bff8 as *const u64;
+                // The frequency given by QEMU is 10_000_000 Hz, so this sets
+                // the next interrupt to fire one second from now.
+                mtimecmp.write_volatile(mtime.read_volatile() + 10_000_000);
+            },
+            11 => {
+                // Machine external (interrupt from Platform Interrupt Controller (PLIC))
+                println!("Machine external interrupt CPU#{}", hartid);
+            }
+            _ => {
+                panic!("Unhandled async trap CPU#{} -> {}\n", hartid, cause_code);
+            }
+        }
+    } else {
+        // Synchronous trap
+        match cause_code {
+            2 => {
+                // Illegal instruction
+                panic!(
+                    "Illegal instruction CPU#{} -> 0x{:08x}: 0x{:08x}\n",
+                    hartid, epc, tval
+                );
+            }
+            8 => {
+                // Environment (system) call from User mode
+                println!("E-call from User mode! CPU#{} -> 0x{:08x}", hartid, epc);
+                program_counter += 4;
+            }
+            9 => {
+                // Environment (system) call from Supervisor mode
+                println!(
+                    "E-call from Supervisor mode! CPU#{} -> 0x{:08x}",
+                    hartid, epc
+                );
+                program_counter += 4;
+            }
+            11 => {
+                // Environment (system) call from Machine mode
+                panic!(
+                    "E-call from Machine mode! CPU#{} -> 0x{:08x}\n",
+                    hartid, epc
+                );
+            }
+            // Page faults
+            12 => {
+                // Instruction page fault
+                println!(
+                    "Instruction page fault CPU#{} -> 0x{:08x}: 0x{:08x}",
+                    hartid, epc, tval
+                );
+                program_counter += 4;
+            }
+            13 => {
+                // Load page fault
+                println!(
+                    "Load page fault CPU#{} -> 0x{:08x}: 0x{:08x}",
+                    hartid, epc, tval
+                );
+                program_counter += 4;
+            }
+            15 => {
+                // Store page fault
+                println!(
+                    "Store page fault CPU#{} -> 0x{:08x}: 0x{:08x}",
+                    hartid, epc, tval
+                );
+                program_counter += 4;
+            }
+            _ => {
+                panic!("Unhandled sync trap CPU#{} -> {}\n", hartid, cause_code);
+            }
+        }
+    };
+    // Finally, return the updated program counter
+    program_counter
 }
 
 #[unsafe(no_mangle)]
