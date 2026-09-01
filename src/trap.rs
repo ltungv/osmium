0450d4976f1a6ce10ac840c3cc0779d8249341f3
ltungv <ltungv.git@gmail.com>
2026-09-01 12:25:48 +0200

    wip: define kernelvec

--- src/trap.rs
+++ src/trap.rs
@@ -1,5 +1,15 @@
+//! Kernel trap handlers.
+
+/// Traps from the supervisor are handled here.
+///
+/// When a trap occurs in supervisor mode, the CPU jumps to `kernelvec` which will in turn call this
+/// function to handle the trap.
 #[unsafe(no_mangle)]
 extern "C" fn kerneltrap() {}
 
+/// Traps from the user are handled here.
+///
+/// When a trap occurs in user mode, the CPU jumps to `uservec` which will in turn call this
+/// function to handle the trap.
 #[unsafe(no_mangle)]
 extern "C" fn usertrap() {}
