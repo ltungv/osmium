a82946f96d8d54b90ba7f100c05ed4779604d775
ltungv <ltungv.git@gmail.com>
2026-09-23 23:30:37 +0200

    wip: refactor build script

--- build.rs
+++ build.rs
@@ -1,29 +1,27 @@
+const ASM_FILES: [&str; 4] = [
+    "src/asm/boot.S",
+    "src/asm/mem.S",
+    "src/asm/trampoline.S",
+    "src/asm/trap.S",
+];
+
 fn main() {
-    println!("cargo:rustc-link-arg=-Wl,--undefined=trampoline");
-    println!("cargo:rustc-link-arg=-Tvirt.ld");
-
-    cc::Build::new()
-        .flags([
-            "-static",
-            "-nostdlib",
-            "-ffreestanding",
-            "-fno-rtti",
-            "-fno-exceptions",
-            "-march=rv64gc",
-            "-mabi=lp64d",
-            "-c",
-            "-march=rv64gc",
-            "-mabi=lp64d",
-        ])
-        .file("src/asm/boot.S")
-        .file("src/asm/mem.S")
-        .file("src/asm/trampoline.S")
-        .file("src/asm/trap.S")
-        .compile("asm");
-
-    println!("cargo:rerun-if-changed=virt.ld");
-    println!("cargo:rerun-if-changed=src/asm/boot.S");
-    println!("cargo:rerun-if-changed=src/asm/mem.S");
-    println!("cargo:rerun-if-changed=src/asm/trampoline.S");
-    println!("cargo:rerun-if-changed=src/asm/trap.S");
+    let mut build = cc::Build::new();
+    build.flags([
+        "-static",
+        "-nostdlib",
+        "-ffreestanding",
+        "-fno-rtti",
+        "-fno-exceptions",
+        "-march=rv64gc",
+        "-mabi=lp64d",
+        "-c",
+        "-march=rv64gc",
+        "-mabi=lp64d",
+    ]);
+    for asm in ASM_FILES {
+        println!("cargo:rerun-if-changed={asm}");
+        build.file(asm);
+    }
+    build.compile("asm");
 }
